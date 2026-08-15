c5bb3bb4351f0df120aa5ce96f2364601aa631f0
Batul J <batuljabalpurwala05@gmail.com>
2026-08-15 14:58:14 +0530

    UI APP

--- AIAssistapp.py
+++ AIAssistapp.py
@@ -1,116 +1,101 @@
 import streamlit as st      # For streamlit web app interface
-import pymupdf              # For PDF text extraction
-from docx import Document   # For DOCX text extraction
-from pptx import Presentation   # For PPTX text extraction
-import io                       # For handling in-memory file operations
+import tempfile             # For creating temporary files
+import os
+from backend import answer_question                   # For interacting with the operating system
+from dotenv import load_dotenv  # For loading environment variables from a .env file
 
-# Text extraction function for various file types
-def extract_text(uploaded_file):
-    # Get the file name and extension from the uploaded file
-    file_name = uploaded_file.name
-    extension = file_name.split(".")[-1].lower()
+load_dotenv()
 
-    try:
-        # For PDF files, use PyMuPDF to extract text
-        if extension == "pdf":
-            doc = pymupdf.open(
-                stream=uploaded_file.getvalue(),        # Open the PDF file from the uploaded file's byte stream
-                filetype="pdf"
-            )
-            text = ""
-            for page_number, page in enumerate(doc, start=1):   # Loop through each page in the PDF starting from page 1
-                page_text = page.get_text()                     # Extract the text from the current page
-
-                text += f"\n\nPage {page_number}\n"             # Add page number header to the extracted text
-                text += page_text
-            doc.close()                                 # Close the PDF file after extraction
-            return text
-
-
-        # For DOCX files, use python-docx to extract text
-        elif extension == "docx":
-
-            document = Document(
-                io.BytesIO(uploaded_file.getvalue())        # Open the DOCX file from the uploaded file's byte stream
-            )
-            text = ""
-            for paragraph in document.paragraphs:       # To extract the text and append it to the text variable
-                text += paragraph.text + "\n"
-            return text
-
-
-        # For PPTX files, use python-pptx to extract text
-        elif extension == "pptx":
-            presentation = Presentation(
-                io.BytesIO(uploaded_file.getvalue())        # Open the PPTX file from the uploaded file's byte stream
-            )
-            text = ""
-            # Loop through each slide in the presentation starting from slide 1
-            for slide_number, slide in enumerate(presentation.slides,start=1):          
-                text += f"\n\nSlide {slide_number}\n"
-                for shape in slide.shapes:
-                    if hasattr(shape, "text"):          # Check if the shape has text attribute
-                        text += shape.text + "\n"
-            return text
-
-        # For txt files, decode the bytes to a string
-        elif extension == "txt":
-            return uploaded_file.getvalue().decode(
-                "utf-8",                    # Decode the byte stream to a UTF-8 string
-                errors="ignore"
-            )
-        else:
-            return "Unsupported file type."
-        
-    except Exception as e:
-
-        return f"ERROR: {str(e)}"       # Return an error message if any exception occurs during text extraction
-
-# Streamlit app configuration 
+# PAGE CONFIGURATION
 st.set_page_config(
-    page_title="AI Research Assistant",
-    page_icon="📚"
+    page_title="AI Assistant",
+    page_icon="🤖",
+    layout="wide"
 )
+# title of the app
+st.title("AI Assistant")
+st.write("Upload your research paper and ask questions about it. And ask questions on them.")
 
-st.title(" _AI Research Assistant_")
-
+# FILE UPLAOD
 uploaded_files = st.file_uploader(
-    "Upload your research files",
+    "Upload your research paper", 
     type=[
         "pdf",
         "docx",
         "pptx",
-        "txt",
-    ],
+        "txt"
+        ], 
     accept_multiple_files=True)
 
+# DISPLAY UPLOADED FILES
 if uploaded_files:
-
-    st.success(
-        f"{len(uploaded_files)} file(s) uploaded successfully!"
-    )
-
+    st.success(f"{len(uploaded_files)} file(s) uploaded successfully!")
     for uploaded_file in uploaded_files:
-        st.subheader(f"{uploaded_file.name}")           # Display the name of each uploaded file as a subheader
-        extracted_text = extract_text(uploaded_file)
+        size_kb = uploaded_file.size / 1024
+        st.write( f"📄 **{uploaded_file.name}** "
+            f"({size_kb:.1f} KB)")
 
-        if extracted_text.startswith("ERROR:"):
-            st.error(extracted_text)        # Error message if text extraction fails
-        elif extracted_text.strip():        # Check if the extracted text is not empty
-            st.success(
-                f"Text extracted successfully — "
-                f"{len(extracted_text)} characters"
-            )
 
-            with st.expander("View extracted text"):        # to view the extracted text in an expandable section
-                st.text_area(
-                    "Extracted Content",
-                    extracted_text,
-                    height=400,
-                    key=f"text_{uploaded_file.name}"
+# CONVERT STREAMLIT UPLOADED FILES TO TEMPORARY FILES
+files_path = []
+temporary_files = []
+try:
+    for uploaded_file in uploaded_files:
+        suffix = os.path.splitext(
+            uploaded_file.name
+            )[1].lower()
+
+        temporary_file = tempfile.NamedTemporaryFile(
+            delete=False, suffix=suffix)
+        
+        temporary_file.write(
+            uploaded_file.getvalue())
+        
+        temporary_file.close()
+
+        # Store the path of the temporary file for later use
+        files_path.append(
+            temporary_file.name)
+
+    st.success("Documents are ready for processing!")
+
+    # QUESTION INPUT
+    st.divider()
+
+    st.subheader("Ask a question about your research paper")
+    question = st.text_area(
+        "Type your question here",
+        placeholder= ("Example: What is the main methodology?"
+                      "used in these research papers?"
+    ),
+    height=100)
+
+    # ASK BUTTON 
+    if st.button(
+        "Ask AI",
+        type = "primary",
+        use_container_width=True):
+        if not question.strip():
+            st.warning("Please enter a question.")
+        else:
+            with st.spinner("Processing your documents and generating an answer..."):
+                answer = answer_question(
+                    question.strip(),
+                    files_path
                 )
 
-        else:
-            st.warning(
-                "No text could be extracted from this file."
-            )
+        # DISPLAY ANSWER
+        st.divider()
+        st.subheader("Answer from AI")
+        st.write(answer)
+
+except Exception as e:
+    st.error(f"An error occurred while processing the files : {str(e)}")
+finally:
+    # CLEAN UP TEMPORARY FILES
+    for file_path in temporary_files:
+        try:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        except Exception :
+            pass  # Ignore errors during cleanup
